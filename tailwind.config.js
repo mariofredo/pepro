d6b7e7461f4905b509b753df77c1ba3486c1295e
mariofredo <mariotannuwijaya@gmail.com>
2026-02-19 14:29:02 +0700

    Refactor theme.liquid and various sections for improved readability and maintainability
    - Updated HTML structure for better formatting and consistency in theme.liquid.
    - Enhanced Liquid code in curated-brands.liquid for improved readability.
    - Cleaned up spacing and formatting in custom-liquid.liquid schema.
    - Improved image handling and added comments in custom-main-product.liquid for clarity.
    - Adjusted button class in buy-buttons.liquid for consistency.
    - Added Tailwind CSS configuration file for better utility management.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  prefix: 'tw',
+  content: [
+    './layout/**/*.liquid',
+    './templates/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
